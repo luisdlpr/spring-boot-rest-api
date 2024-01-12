9ab71ed7edbc92f7c4a3fd955c334a5d2dc45d22
luisdlpr <reyesluichi@hotmail.com>
2024-01-12 21:29:12 +1100

    use student class in get endpoint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,16 @@
 // 2110 https://www.youtube.com/watch?v=9SGDpanrc8U
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
 @RestController
@@ -16,7 +20,8 @@ public class DemoApplication {
     }
 
     @GetMapping
-    public List<String> hello() {
-        return List.of("Hello", "World");
+    public List<Student> hello() {
+        return List.of(
+                new Student(1L, "Mariam", "mariam@gmail.com", 23, LocalDate.of(2000, Month.JANUARY, 21)));
     }
 }
